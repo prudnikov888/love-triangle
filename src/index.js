8f41cd376eddcd4a1a05af5de69c0abff38b5237
Andrei Prudnikau <prudnikov888@gmail.com>
2018-09-11 00:29:19 +0300

    Love-triangle

--- src/index.js
+++ src/index.js
@@ -3,5 +3,26 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var count = 0;
+  for (i=0; i < preferences.length; i++) {
+      var firstLoveNumber = preferences[i];
+      var firstLoveIndex = firstLoveNumber - 1;
+      if (i === firstLoveIndex) {
+          continue;
+      }
+      var secondLoveNumber = preferences[firstLoveIndex];
+      var secondLoveIndex = secondLoveNumber - 1;
+      if (firstLoveIndex === secondLoveIndex) {
+          continue;
+      }
+      var thirdLoveNumber = preferences[secondLoveIndex];
+      var thirdLoveIndex = thirdLoveNumber - 1;
+      if (secondLoveIndex === thirdLoveIndex) {
+          continue;
+      }
+      if (i === thirdLoveIndex) {
+          count++;
+      }
+  }
+  return count/3;
 };
